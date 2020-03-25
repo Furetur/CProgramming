7729562605f9113357e11934eb6459165a9c99d2
furetur <furetur@gmail.com>
2020-03-25 12:12:43 +0500

    arrayutils@latest

---
+++ lib/arrayutils/arrayutils.h
@@ -0,0 +1,64 @@
+//
+// Created by furet on 26.02.2020.
+//
+#include <stdbool.h>
+
+#ifndef HOMEWORK_ARRAYUTILS_H
+#define HOMEWORK_ARRAYUTILS_H
+
+
+void fillCharArray(char* array, int size, char val);
+
+void emptyCharArray(char* array, int size);
+
+char* createCharArrayWithVal(int size, char defaultVal);
+
+char* createCharArray(int size);
+
+int* createIntArray(int size);
+
+int* createIntArrayWithVal(int size, int defaultVal);
+
+bool* createBoolArray(int size);
+
+bool* createBoolArrayWithVal(int size, bool val);
+
+int** createInt2dArrayWithVal(int numRows, int numCols, int val);
+
+int** createInt2dArray(int numRows, int numCols);
+
+void readInt2dArray(int** arr, int numRows, int numCols);
+
+bool** createBool2dArray(int numRows, int numCols);
+
+bool** createBool2dArrayWithVal(int numRows, int numCols, bool val);
+
+void freeInt2dArray(int** array, int numRows);
+
+void freeBool2dArray(bool** array, int numRows);
+
+void readIntArray(int arr[], int size);
+
+void printIntArray(const int arr[], int size);
+
+void emptyIntArray(int arr[], int size);
+
+char* copyCharArray(char* array);
+
+void writeCharArrayIntoArray(char* parentArray, char* childArray, int parentArrayStartIndex);
+
+void reverseIntArray(int arr[], int start, int end);
+
+bool areArraysEqual(const char* arr1, const char* arr2, int size);
+
+char* getSubarray(const char* array, int start, int end);
+
+int* copyIntArray(const int* array, int size);
+
+void quickSort(int* arr,int startIndex,int endIndex);
+
+bool allTruthy(const bool* arr, int size);
+
+int findFirstInBoolArray(const bool* arr, bool val, int size);
+
+#endif //HOMEWORK_ARRAYUTILS_H
