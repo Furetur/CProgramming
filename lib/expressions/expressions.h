978f223be9b01f7aba41e97709d77719458c692b
furetur <furetur@gmail.com>
2020-03-25 13:21:12 +0500

    expressions@5.2

---
+++ lib/expressions/expressions.h
@@ -0,0 +1,13 @@
+//
+// Created by furetur on 07.03.2020.
+//
+
+#ifndef HOMEWORK_EXPRESSIONS_H
+#define HOMEWORK_EXPRESSIONS_H
+
+
+int evaluatePostfixExpression(char expression[]);
+
+char* convertInfixToPostfix(char* expression);
+
+#endif //HOMEWORK_EXPRESSIONS_H
