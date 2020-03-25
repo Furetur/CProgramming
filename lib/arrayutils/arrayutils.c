66c3ea1fbb28ebf623b163d73c0fea43d8230a66
furetur <furetur@gmail.com>
2020-03-25 13:47:07 +0500

    arrayutils@latest.1 (code style)

--- lib/arrayutils/arrayutils.c
+++ lib/arrayutils/arrayutils.c
@@ -184,7 +184,8 @@ void emptyIntArray(int arr[], const int size)
 
 void writeCharArrayIntoArray(char* parentArray, char* childArray, int parentArrayStartIndex)
 {
-    for (int i = 0; i < strlen(childArray); ++i) {
+    for (int i = 0; i < strlen(childArray); ++i)
+    {
         parentArray[i + parentArrayStartIndex] = childArray[i];
     }
 }
@@ -194,7 +195,8 @@ char* copyCharArray(char* array)
 {
     const int length = strlen(array);
     char* destination = createCharArray(length + 1);
-    for (int i = 0; i < length; ++i) {
+    for (int i = 0; i < length; ++i)
+    {
         destination[i] = array[i];
     }
     return destination;
@@ -309,7 +311,8 @@ void quickSort(int* arr,const int startIndex,const int endIndex)
 
 bool allTruthy(const bool* arr, const int size)
 {
-    for (int i = 0; i < size; ++i) {
+    for (int i = 0; i < size; ++i)
+    {
         if (arr[i] == false)
         {
             return false;
@@ -321,7 +324,8 @@ bool allTruthy(const bool* arr, const int size)
 
 bool allFalsy(const bool* arr, const int size)
 {
-    for (int i = 0; i < size; ++i) {
+    for (int i = 0; i < size; ++i)
+    {
         if (arr[i] == true)
         {
             return false;
@@ -333,7 +337,8 @@ bool allFalsy(const bool* arr, const int size)
 
 int findFirstInBoolArray(const bool* arr, const bool val, const int size)
 {
-    for (int i = 0; i < size; ++i) {
+    for (int i = 0; i < size; ++i)
+    {
         if (arr[i] == val)
         {
             return i;
