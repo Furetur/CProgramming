9809323865332112d8e4623a2e65ca8d1d0dd278
furetur <furetur@gmail.com>
2020-03-25 13:42:16 +0500

    commonutils@latest

---
+++ lib/commonutils/commonutils.h
@@ -0,0 +1,14 @@
+//
+// Created by furet on 26.02.2020.
+//
+
+#ifndef HOMEWORK_COMMONUTILS_H
+#define HOMEWORK_COMMONUTILS_H
+
+void swapInts(int *num1, int *num2);
+
+int min(int num1, int num2);
+
+int max(int num1, int num2);
+
+#endif //HOMEWORK_COMMONUTILS_H
