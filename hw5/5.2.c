b68d22583560d3582a73f81923b05149efeb846a
furetur <furetur@gmail.com>
2020-05-26 20:49:50 +0500

    5.2 added error handling

--- hw5/5.2.c
+++ hw5/5.2.c
@@ -2,8 +2,9 @@
 // Created by furetur on 06.03.2020.
 //
 #include "stdio.h"
-#include "../lib/arrayutils/arrayutils.h"
+#include "errno.h"
 #include "stdlib.h"
+#include "../lib/arrayutils/arrayutils.h"
 #include "../lib/expressions/expressions.h"
 
 
@@ -17,9 +18,22 @@ int main()
     printf("Enter the expression in the postfix form:\n");
     scanf("%[^\n]", postfixExpression);
 
-    const int result = evaluatePostfixExpression(postfixExpression);
-
-    printf("Evaluation result: %d\n", result);
+    if (isPostfixExpressionValid(postfixExpression))
+    {
+        const int result = evaluatePostfixExpression(postfixExpression);
+        if (errno == 22)
+        {
+            printf("You tried to divide by zero -_-");
+        }
+        else
+        {
+            printf("Evaluation result: %d\n", result);
+        }
+    }
+    else
+    {
+        printf("The expression you entered is not valid");
+    }
 
     free(postfixExpression);
     return 0;
