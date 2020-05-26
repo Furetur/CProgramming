cfc2339006e6db5430b5e62c11a56fee3692c347
furetur <furetur@gmail.com>
2020-05-26 21:10:17 +0500

    commonutils changed max & min implementation

--- lib/commonutils/commonutils.c
+++ lib/commonutils/commonutils.c
@@ -15,15 +15,11 @@ void swapInts(int *num1, int *num2)
 
 int min(const int num1, const int num2)
 {
-    if (num1 < num2)
-    {
-        return num1;
-    }
-    return num2;
+    return num1 < num2 ? num1 : num2;
 }
 
 
 int max(const int num1, const int num2)
 {
-    return -1 * min(-1 * num1, -1 * num2);
+    return num1 > num2 ? num1 : num2;
 }
