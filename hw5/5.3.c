9aa96ebe16dc8d62ef8bdf68020fd156fa965f41
furetur <furetur@gmail.com>
2020-06-01 17:17:18 +0500

    better error handling

--- hw5/5.3.c
+++ hw5/5.3.c
@@ -3,12 +3,13 @@
 //
 
 #include <stdlib.h>
+#include <errno.h>
 #include "../lib/arrayutils/arrayutils.h"
 #include "../lib/expressions/expressions.h"
 #include "stdio.h"
+#include "../lib/stringutils/stringutils.h"
 
 const int EXPRESSION_MAX_SIZE = 500;
-
 
 int main()
 {
@@ -17,16 +18,33 @@ int main()
     printf("Enter the expression in the infix form (please do not use whitespaces):\n");
     scanf("%[^\n]", infixExpression);
 
-    char* postfixExpression = convertInfixToPostfix(infixExpression);
+    if (!isBalanced(infixExpression))
+    {
+        printf("Wrong combination of parenthesis");
+    }
+    else
+    {
+        char* postfixExpression = convertInfixToPostfix(infixExpression);
 
-    printf("Your expression in the postfix form: %s\n", postfixExpression);
-
-    const int result = evaluatePostfixExpression(postfixExpression);
-
-    printf("Evaluation result: %d\n", result);
-
+        if (isPostfixExpressionValid(postfixExpression))
+        {
+            printf("Your expression in the postfix form: %s\n", postfixExpression);
+            const int result = evaluatePostfixExpression(postfixExpression);
+            if (errno == 22)
+            {
+                printf("You tried to divide by zero -_-");
+            }
+            else
+            {
+                printf("Evaluation result: %d\n", result);
+            }
+        }
+        else
+        {
+            printf("The expression you entered is not valid");
+        }
+        free(postfixExpression);
+    }
     free(infixExpression);
-    free(postfixExpression);
-
     return 0;
 }
